5903e07ab4d59d3b21e4ce9260eeb95570fa5d94
haldrby28-gif <haldrby28@gmail.com>
2026-07-13 05:25:42 +0300

    Update orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,1 +1,79 @@
 
+import { db } from "../../js/firebase.js";
+
+import {
+
+collection,
+getDocs,
+doc,
+updateDoc
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const table=document.getElementById("ordersTable");
+
+async function loadOrders(){
+
+table.innerHTML="";
+
+const snapshot=await getDocs(collection(db,"orders"));
+
+snapshot.forEach(order=>{
+
+const data=order.data();
+
+table.innerHTML+=`
+
+<tr>
+
+<td>${order.id}</td>
+
+<td>${data.userId}</td>
+
+<td>${data.total} جنيه</td>
+
+<td>
+
+<select onchange="changeStatus('${order.id}',this.value)">
+
+<option ${data.status=="pending"?"selected":""} value="pending">جديد</option>
+
+<option ${data.status=="preparing"?"selected":""} value="preparing">جارى التحضير</option>
+
+<option ${data.status=="delivery"?"selected":""} value="delivery">مع المندوب</option>
+
+<option ${data.status=="completed"?"selected":""} value="completed">تم التسليم</option>
+
+<option ${data.status=="cancelled"?"selected":""} value="cancelled">ملغى</option>
+
+</select>
+
+</td>
+
+<td>
+
+✅
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+window.changeStatus=async(id,status)=>{
+
+await updateDoc(doc(db,"orders",id),{
+
+status:status
+
+});
+
+alert("تم تحديث حالة الطلب");
+
+}
+
+loadOrders();
