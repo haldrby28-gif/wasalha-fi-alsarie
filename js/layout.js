254179af0becb47b7617725183ab60ca3e443642
haldrby28-gif <haldrby28@gmail.com>
2026-07-13 06:02:47 +0300

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -1,9 +1,26 @@
 async function loadComponent(id, file) {
-    const response = await fetch(file);
-    const html = await response.text();
-    document.getElementById(id).innerHTML = html;
+    const res = await fetch(file);
+    document.getElementById(id).innerHTML = await res.text();
+
+    if (id === "sidebar") {
+        activateCurrentPage();
+    }
 }
 
 loadComponent("sidebar", "./components/sidebar.html");
 loadComponent("header", "./components/header.html");
 loadComponent("footer", "./components/footer.html");
+
+function activateCurrentPage() {
+    const page = location.pathname.split("/").pop();
+
+    document.querySelectorAll(".sidebar a").forEach(link => {
+        if (link.getAttribute("href") === page) {
+            link.classList.add("active");
+        }
+    });
+}
+
+window.toggleSidebar = function () {
+    document.querySelector(".sidebar").classList.toggle("show");
+};
