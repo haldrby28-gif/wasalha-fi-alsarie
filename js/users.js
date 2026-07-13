3c654b47b72102dad8392f2ef7aa33fe800a5451
haldrby28-gif <haldrby28@gmail.com>
2026-07-13 05:33:39 +0300

    Update users.js

--- js/users.js
+++ js/users.js
@@ -1,1 +1,44 @@
+import { db } from "../../js/firebase.js";
 
+import {
+collection,
+onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const table = document.getElementById("usersTable");
+
+onSnapshot(collection(db,"users"),(snapshot)=>{
+
+table.innerHTML="";
+
+snapshot.forEach((user)=>{
+
+const data=user.data();
+
+table.innerHTML += `
+
+<tr>
+
+<td>${data.name || "-"}</td>
+
+<td>${data.email || "-"}</td>
+
+<td>${data.role || "customer"}</td>
+
+<td>
+
+<span class="status ${data.active ? "active":"inactive"}">
+
+${data.active ? "نشط":"موقوف"}
+
+</span>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+});
