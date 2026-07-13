e5007e9deb46056b7b12fc73e45a867000e14b87
haldrby28-gif <haldrby28@gmail.com>
2026-07-13 22:18:09 +0300

    Update home.js

--- js/driver/home.js
+++ js/driver/home.js
@@ -1,1 +1,69 @@
+import { auth, db } from "../firebase.js";
 
+import {
+    doc,
+    getDoc,
+    updateDoc,
+    collection,
+    query,
+    where,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const driverName = document.getElementById("driverName");
+const driverStatus = document.getElementById("driverStatus");
+const ordersContainer = document.getElementById("ordersContainer");
+
+auth.onAuthStateChanged(async (user) => {
+
+    if (!user) {
+
+        window.location.href = "login.html";
+        return;
+
+    }
+
+    const driverRef = doc(db, "drivers", user.uid);
+    const driverSnap = await getDoc(driverRef);
+
+    if (!driverSnap.exists()) {
+
+        alert("هذا الحساب ليس مندوب.");
+
+        window.location.href = "login.html";
+
+        return;
+
+    }
+
+    const driver = driverSnap.data();
+
+    driverName.textContent = driver.name;
+
+    driverStatus.textContent =
+        driver.isOnline ? "🟢 متصل" : "🔴 غير متصل";
+
+    loadOrders(user.uid);
+
+});
+
+async function loadOrders(driverId) {
+
+    const q = query(
+        collection(db, "orders"),
+        where("status", "==", "جديد")
+    );
+
+    const snapshot = await getDocs(q);
+
+    ordersContainer.innerHTML = "";
+
+    if (snapshot.empty) {
+
+        ordersContainer.innerHTML = `
+        <p style="text-align:center;">
+            لا توجد طلبات جديدة.
+        </p>
+        `;
+
+       
