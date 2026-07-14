e64b50d2a7c917ef0380107532710e1d253d1bef
haldrby28-gif <haldrby28@gmail.com>
2026-07-14 21:11:44 +0300

    Update users.js

--- js/users.js
+++ js/users.js
@@ -48,4 +48,77 @@ async function loadUsers() {
         tbody.innerHTML = `
             <tr>
                 <td colspan="5">حدث خطأ أثناء تحميل البيانات.</td>
-           
+            </tr>
+        `;
+
+    }
+
+}
+
+function renderUsers(list) {
+
+    tbody.innerHTML = "";
+
+    if (list.length === 0) {
+
+        tbody.innerHTML = `
+            <tr>
+                <td colspan="5">
+                    لا يوجد مستخدمون.
+                </td>
+            </tr>
+        `;
+
+        return;
+
+    }
+
+    list.forEach(user => {
+
+        tbody.innerHTML += `
+
+        <tr>
+
+            <td>${user.name || "-"}</td>
+
+            <td>${user.email || "-"}</td>
+
+            <td>${user.role || "customer"}</td>
+
+            <td>
+
+                <span class="status ${user.disabled ? "inactive" : "active"}">
+
+                    ${user.disabled ? "موقوف" : "نشط"}
+
+                </span>
+
+            </td>
+
+            <td>
+
+                <button
+                    class="toggle"
+                    onclick="toggleUser('${user.id}', ${user.disabled ? true : false})">
+
+                    ${user.disabled ? "تفعيل" : "إيقاف"}
+
+                </button>
+
+                <button
+                    class="delete"
+                    onclick="removeUser('${user.id}')">
+
+                    حذف
+
+                </button>
+
+            </td>
+
+        </tr>
+
+        `;
+
+    });
+
+}
