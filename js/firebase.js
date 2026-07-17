e3aa78f1dce8d030e6302900088ea7f855fe7d40
haldrby28-gif <haldrby28@gmail.com>
2026-07-17 17:50:00 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,26 @@
+// js/firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Firebase Configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCWsZP1x77VWX_AFCwboJH8vjZ5V8i0Vt0",
+  authDomain: "wassalha-afe6b.firebaseapp.com",
+  projectId: "wassalha-afe6b",
+  storageBucket: "wassalha-afe6b.firebasestorage.app",
+  messagingSenderId: "211540847218",
+  appId: "1:211540847218:web:b22b25792baff28adb30eb",
+  measurementId: "G-K0MBK3NSEB"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Firebase Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export Services
+export { app, auth, db };
